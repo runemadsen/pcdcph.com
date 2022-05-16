3336cfa5cb5902e7105303b483b416e4f75dd689
Rune Madsen <rune@runemadsen.com>
2022-05-16 13:30:16 +0200

    more

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,44 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Processing Community Day Copenhagen",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Processing Community Day Copenhagen'
   },
   plugins: [
     {
       resolve: `gatsby-plugin-postcss`,
       options: {
         postCssPlugins: [
-          require("postcss-nesting"),
-          require("postcss-custom-properties")({
-            importFrom: "./src/styles/variables.css",
+          require('postcss-nesting'),
+          require('postcss-custom-properties')({
+            importFrom: './src/styles/variables.css'
           }),
-          require("postcss-calc")(),
-          require("postcss-custom-media")({
-            importFrom: "./src/styles/variables.css",
-          }),
-        ],
-      },
+          require('postcss-calc')(),
+          require('postcss-custom-media')({
+            importFrom: './src/styles/variables.css'
+          })
+        ]
+      }
     },
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/'
       },
-      __key: "images",
+      __key: 'images'
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: 'pages',
+        path: './src/pages/'
       },
-      __key: "pages",
-    },
-  ],
+      __key: 'pages'
+    }
+  ]
 };
